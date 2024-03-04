6cd6eadd594199cedd048e8141dd794230064e99
Hiran <134024847+hiranKaushalR@users.noreply.github.com>
2024-03-04 19:37:29 +0530

    3rd commit

--- src/Pages/Contact/ContactBody.jsx
+++ src/Pages/Contact/ContactBody.jsx
@@ -1,24 +1,47 @@
-import React from 'react'
-import { contactDetails } from '../../Constant'
+import React from "react";
+import { contactDetails } from "../../Constant";
 
 function ContactBody() {
   return (
-    <div  className='contact-container--body'>
-        <div className='contact-container--body--contact-details'>
-            {
-                contactDetails.map ((contactDetail) => (
-                    <div>
-                        <p>{contactDetail.type}</p>
-                        <p>{contactDetail.detail}</p>
-                    </div>
-                ))
-            }
+    <div className="contact-container--body">
+      <div className="contact-container--body--contact-details">
+        {contactDetails.map((contactDetail) => (
+          <div>
+            <p className="type">{contactDetail.type}</p>
+            <p className="detail">{contactDetail.detail}</p>
+          </div>
+        ))}
+      </div>
+      <form>
+        <div className="form-first-half">
+          <div className="form-first-half-name">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+          <div className="form-first-half-mobile">
+            <label htmlFor="mobile">Mobie</label>
+            <input type="number" name="mobile" id="mobile" />
+          </div>
         </div>
-        <form >
-
-        </form>
+        <div className="form-second-half">
+          <div className="">
+            <label htmlFor="mobile-number">Mobile Number</label>
+            <input type="number" name="mobile-number" id="mobile-number" />
+          </div>
+          <div className="">
+            {" "}
+            <label htmlFor="email">Email</label>
+            <input type="email" name="email" id="email" />
+          </div>
+          <div className="">
+            {" "}
+            <label htmlFor="message">Message</label>
+            <input type="text" id="message" />
+          </div>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default ContactBody
+export default ContactBody;
