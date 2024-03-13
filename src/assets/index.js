f1f02bd9cd90ef0c19246082e7701f78ae42a543
Hiran <134024847+hiranKaushalR@users.noreply.github.com>
2024-03-13 17:10:01 +0530

    fixed the service page icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -38,7 +38,7 @@ import serviceBackground from "./service-background.png";
 import service1 from "./service-1.png";
 import service2 from "./service-2.png";
 import service3 from "./service-3.png";
-import service4 from "./service-4.png";
+import service4 from "./service-4.svg";
 
 export { serviceBackground, service1, service2, service3, service4 };
 
